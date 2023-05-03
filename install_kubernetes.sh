a785ac2118be7345efaad4b77c2434d65a1f1a7c
diranetafen <diranetafen@yahoo.com>
2023-05-03 06:51:45 +0000

    add kubectl completion

--- install_kubernetes.sh
+++ install_kubernetes.sh
@@ -15,6 +15,7 @@ ansible-galaxy install -r roles/requirements.yml
 if [ $1 == "master" ]
 then
         ansible-playbook install_kubernetes.yml --extra-vars "kubernetes_role=control_plane kubernetes_apiserver_advertise_address=$2 kubernetes_version_rhel_package='$KUBERNETES_VERSION'"
+        yum install bash-completion -y && kubectl completion bash | sudo tee /etc/bash_completion.d/kubectl > /dev/null
         echo "###################################################"
         echo "For this Stack, you will use $(ip -f inet addr show enp0s8 | sed -En -e 's/.*inet ([0-9.]+).*/\1/p') IP Address"
         echo "You need to be root to use kubectl in $(ip -f inet addr show enp0s8 | sed -En -e 's/.*inet ([0-9.]+).*/\1/p') VM (run 'sudo su -' to become root and then use kubectl as you want)"
