245f9dbcbab19f104cdd8f60ffee006b0217c76c
InvisibleReMedia <33389140+InvisibleReMedia@users.noreply.github.com>
2023-12-31 07:07:50 +0100

    Update install_kubernetes.sh
    removed --ignore-preflight-errors=all
    as long as, preflight errors may be use at your own risk (if you know what you are doing).

--- install_kubernetes.sh
+++ install_kubernetes.sh
@@ -21,6 +21,6 @@ then
         echo "You need to be root to use kubectl in $(ip -f inet addr show enp0s8 | sed -En -e 's/.*inet ([0-9.]+).*/\1/p') VM (run 'sudo su -' to become root and then use kubectl as you want)"
         echo "###################################################"
 else
-        ansible-playbook install_kubernetes.yml --extra-vars "kubernetes_role=$1 kubernetes_apiserver_advertise_address=$2 kubernetes_version_rhel_package='$KUBERNETES_VERSION' kubernetes_join_command='kubeadm join {{ kubernetes_apiserver_advertise_address }}:6443 --ignore-preflight-errors=all --token={{ token }}  --discovery-token-unsafe-skip-ca-verification'"
+        ansible-playbook install_kubernetes.yml --extra-vars "kubernetes_role=$1 kubernetes_apiserver_advertise_address=$2 kubernetes_version_rhel_package='$KUBERNETES_VERSION' kubernetes_join_command='kubeadm join {{ kubernetes_apiserver_advertise_address }}:6443 --token={{ token }}  --discovery-token-unsafe-skip-ca-verification'"
         echo "For this Stack, you will use $(ip -f inet addr show enp0s8 | sed -En -e 's/.*inet ([0-9.]+).*/\1/p') IP Address"
 fi
