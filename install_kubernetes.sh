34a442906a07a560832683531b97ddf607495a77
Tchassem Boutchouang Anselme Gildas <aansegildass@yahoo.fr>
2026-09-09 09:08:25 +0100

    fix: corrige les chemins yaml/yml et le nom du role Galaxy dans le provisioning
    - install_kubernetes.sh referencait requirements.yaml et install_kubernetes.yaml
      alors que les fichiers du repo sont en .yml, ce qui faisait echouer vagrant up
    - ajoute un DPkg::Lock::Timeout pour attendre le verrou dpkg au lieu d'echouer
      quand unattended-upgrades tourne encore au premier boot
    - roles/requirements.yml installait le role sous le nom ulrich-sun.kubernetes
      alors que le playbook et le script attendent le nom local kubernetes

--- install_kubernetes.sh
+++ install_kubernetes.sh
@@ -7,6 +7,10 @@ MASTER_IP=$2
 echo "Role: $ROLE"
 echo "Master IP: $MASTER_IP"
 
+# Attendre le verrou dpkg au lieu d'échouer immédiatement si
+# unattended-upgrades tourne encore au premier boot de la VM
+echo 'DPkg::Lock::Timeout "600";' | sudo tee /etc/apt/apt.conf.d/99-dpkg-lock-timeout > /dev/null
+
 # Mettre à jour les paquets
 sudo apt update -y
 # sudo apt upgrade -y
@@ -19,15 +23,15 @@ sudo apt -y install ansible git
 
 # Installer le rôle depuis Galaxy
 ansible-galaxy remove kubernetes
-ansible-galaxy install -r /vagrant/roles/requirements.yaml
+ansible-galaxy install -r /vagrant/roles/requirements.yml
 
 export K8S_MASTER_IP=$MASTER_IP
 
 # Lancer le playbook
 if [ "$1" == "master" ]; then
-    ansible-playbook /vagrant/install_kubernetes.yaml \
+    ansible-playbook /vagrant/install_kubernetes.yml \
     --extra-vars "kubernetes_role=$ROLE kubernetes_apiserver_advertise_address=$K8S_MASTER_IP installation_method=vagrant"
 else
-    ansible-playbook /vagrant/install_kubernetes.yaml \
+    ansible-playbook /vagrant/install_kubernetes.yml \
       --extra-vars "kubernetes_role=$ROLE kubernetes_apiserver_advertise_address=$K8S_MASTER_IP installation_method=vagrant kubernetes_join_command='kubeadm join $K8S_MASTER_IP:6443 --ignore-preflight-errors=all --token={{ token }} --discovery-token-unsafe-skip-ca-verification'"
 fi
